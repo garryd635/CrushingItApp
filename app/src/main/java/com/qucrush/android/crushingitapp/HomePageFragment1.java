4687e344f87b5a3abc42c94c7346fd33d2410d6e
garryd635 <garryd635@gmail.com>
2017-05-01 11:06:43 -0400

    Home screen 1's progress bar changes according to task completion on the current day.  Added textview to display percentage of progress.

--- app/src/main/java/com/qucrush/android/crushingitapp/HomePageFragment1.java
+++ app/src/main/java/com/qucrush/android/crushingitapp/HomePageFragment1.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,7 +20,8 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * Created by Garry on 2/8/2017.
+ * Home Page Fragment
+ *  The screen that contains home_1.xml as part of the screens for the homepage
  */
 
 public class HomePageFragment1 extends Fragment{
@@ -25,6 +29,7 @@ public class HomePageFragment1 extends Fragment{
     View myView;
     String type = "one";
     ProgressBar dayStatus;
+    TextView percentText;
     List<Task> allTasks,todayTasks;
     LayoutInflater inflater;
     ViewGroup container;
@@ -34,21 +39,35 @@ public class HomePageFragment1 extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         this.inflater = inflater;
         this.container = container;
-//        allTasks = MainActivity.tm.retrieveTasks();
-//        todayTasks = new ArrayList<Task>();
-//        todayTasks = getTodayTasksList(allTasks);
-//        dayStatus = (ProgressBar) myView.findViewById(R.id.progressBar);
-//        totalPer = todayTasks.size();
-//        completePer = 0;
-//
-//        for(int i =0; i < todayTasks.size();i++){
-//            if(todayTasks.get(i).getCompletion().equals("yes")){
-//                completePer++;
-//            }
-//        }
-
-
         myView = inflater.inflate(R.layout.home_1, container, false);
+
+        allTasks = MainActivity.tm.retrieveTasks();
+        todayTasks = new ArrayList<Task>();
+        todayTasks = getTodayTasksList(allTasks);
+        dayStatus = (ProgressBar) myView.findViewById(R.id.homeProgressBar);
+        percentText = (TextView) myView.findViewById(R.id.percentText);
+        totalPer = todayTasks.size();
+        completePer = 0;
+
+        //If there's tasks for today, calculate the total completed tasks
+        if(todayTasks.size() > 0){
+            for(int i =0; i < todayTasks.size();i++){
+                if(todayTasks.get(i).getCompletion().equals("yes")){
+                    completePer++;
+                }
+            }
+        }
+
+        if(completePer != 0){
+            dayStatus.setProgress(((completePer*10/totalPer*10)));
+            percentText.setText((completePer*10/totalPer*10) + "%");
+        }else if(todayTasks != null){
+            dayStatus.setProgress(0);
+            percentText.setText("0 %");
+        }else{
+            dayStatus.setProgress(0);
+            percentText.setText("No Tasks Today");
+        }
 
         return myView;
     }
@@ -58,28 +77,25 @@ public class HomePageFragment1 extends Fragment{
      *
      * @return tempLst
      */
-    //public List<Task> getTodayTasksList(List<Task> taskList){
-//        List<Task> tempLst = new ArrayList<Task>();
-//        Calendar todayCal = Calendar.getInstance();
-//        Calendar taskCal = Calendar.getInstance();
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        for(int i = 0; i < taskList.size(); i++){
-//            try{
-//                todayCal.setTime(sdf.parse(taskList.get(i).getDate()));
-//            }catch (ParseException e){
-//                System.out.println("***ERROR***");
-//            }
-//            if((todayCal.get(Calendar.YEAR) == taskCal.get(Calendar.YEAR)) &&
-//                    (todayCal.get(Calendar.MONTH) == taskCal.get(Calendar.MONTH)) &&
-//                    (todayCal.get(Calendar.DAY_OF_MONTH) == taskCal.get(Calendar.DAY_OF_MONTH))){
-//                tempLst.add(taskList.get(i));
-//            }
-//        }
-
-        //return tempLst;
-//    }//setTodayTaskList()
-
-    public void setParameter(String toType) {
-        type = toType;
-    }
-}
+    public List<Task> getTodayTasksList(List<Task> taskList){
+        List<Task> tempLst = new ArrayList<Task>();
+        Calendar todayCal = Calendar.getInstance();
+        Calendar taskCal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        if(taskList != null) {
+            for (int i = 0; i < taskList.size(); i++) {
+                try {
+                    todayCal.setTime(sdf.parse(taskList.get(i).getDate()));
+                } catch (ParseException e) {
+                    System.out.println("***ERROR***");
+                }
+                if ((todayCal.get(Calendar.YEAR) == taskCal.get(Calendar.YEAR)) &&
+                        (todayCal.get(Calendar.MONTH) == taskCal.get(Calendar.MONTH)) &&
+                        (todayCal.get(Calendar.DAY_OF_MONTH) == taskCal.get(Calendar.DAY_OF_MONTH))) {
+                    tempLst.add(taskList.get(i));
+                }
+            }
+        }
+        return tempLst;
+    }//setTodayTaskList()
+}//HomePageFragment1
