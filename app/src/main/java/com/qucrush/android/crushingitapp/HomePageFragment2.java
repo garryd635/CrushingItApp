136c68e05155c9e85754644ffec2786d7ff7ad2f
garryd635 <garryd635@gmail.com>
2017-05-01 12:58:15 -0400

    Homepage 3 now displays completed tasks for the current day.  Added code that will contribute to the upcoming task list (not implemented yet).

--- app/src/main/java/com/qucrush/android/crushingitapp/HomePageFragment2.java
+++ app/src/main/java/com/qucrush/android/crushingitapp/HomePageFragment2.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 
 /**
  * Created by Garry on 2/8/2017.
@@ -14,23 +15,18 @@ import android.widget.Button;
 
 public class HomePageFragment2 extends Fragment{
     View myView;
-    String type = "one";
-    Button next1, prev1, next2, prev2, next3, prev3;
     LayoutInflater inflater;
     ViewGroup container;
+    ListView upcomingList;
+    CustomAdapter adapter;
     @Nullable
     //@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         this.inflater = inflater;
         this.container = container;
-
         myView = inflater.inflate(R.layout.home_2, container, false);
+        upcomingList = (ListView) myView.findViewById(R.id.homeUpcomingList);
 
         return myView;
-    }
-
-    public void setParameter(String toType) {
-        type = toType;
     }
 }
--- app/src/main/java/com/qucrush/android/crushingitapp/HomePageFragment3.java
+++ app/src/main/java/com/qucrush/android/crushingitapp/HomePageFragment3.java
@@ -3,10 +3,18 @@ package com.qucrush.android.crushingitapp;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by Garry on 2/8/2017.
@@ -14,23 +22,54 @@ import android.widget.Button;
 
 public class HomePageFragment3 extends Fragment{
     View myView;
-    String type = "one";
-    Button next1, prev1, next2, prev2, next3, prev3;
     LayoutInflater inflater;
     ViewGroup container;
+    ListView completeLst;
+    List<Task> complete;
+    CompletionAdapter dataAdapter;
     @Nullable
     //@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         this.inflater = inflater;
         this.container = container;
-
         myView = inflater.inflate(R.layout.home_3, container, false);
+        completeLst = (ListView) myView.findViewById(R.id.homeCompleteList);
+        complete = getTodayCompletedTasksList(MainActivity.tm.retrieveTasks());
+
+        if(complete != null){
+            dataAdapter = new CompletionAdapter(getActivity(),
+                    R.layout.home_3, complete);
+            completeLst.setAdapter(dataAdapter);
+        }
 
         return myView;
     }
-
-    public void setParameter(String toType) {
-        type = toType;
-    }
+    /**
+     * Gets all the tasks that are due on the current day
+     *
+     * @return tempLst
+     */
+    public List<Task> getTodayCompletedTasksList(List<Task> taskList){
+        List<Task> tempLst = new ArrayList<Task>();
+        Calendar todayCal = Calendar.getInstance();
+        Calendar taskCal = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        if(taskList != null) {
+            for (int i = 0; i < taskList.size(); i++) {
+                try {
+                    todayCal.setTime(sdf.parse(taskList.get(i).getDate()));
+                } catch (ParseException e) {
+                    System.out.println("***ERROR***");
+                }
+                if ((todayCal.get(Calendar.YEAR) == taskCal.get(Calendar.YEAR)) &&
+                        (todayCal.get(Calendar.MONTH) == taskCal.get(Calendar.MONTH)) &&
+                        (todayCal.get(Calendar.DAY_OF_MONTH) == taskCal.get(Calendar.DAY_OF_MONTH))) {
+                    if(taskList.get(i).getCompletion().equals("yes")){
+                        tempLst.add(taskList.get(i));
+                    }
+                }
+            }
+        }
+        return tempLst;
+    }//setTodayTaskList()
 }
